5354a1d5efc4ca60bfc2464f065523bfe3bf0789
Soliunas <simas.soliunas@gmail.com>
2023-07-28 23:34:21 +0300

    Account layout

--- src/layout/AccountLayout.jsx
+++ src/layout/AccountLayout.jsx
@@ -1,2 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import style from '../pages/Registration.module.css';
+
+export function AccountLayout() {
+    return (
+        <div>
+            <header>
+                ACCOUNT HEADER
+            </header>
+            <Outlet />
+        </div>
+    );
+}
