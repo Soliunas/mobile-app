e13695a24cc733c6209dc42d6323250a91ad0ec5
Soliunas <simas.soliunas@gmail.com>
2023-07-28 23:31:40 +0300

    Authentification layout

---
+++ src/layout/AuthentificationLayout.jsx
@@ -0,0 +1,36 @@
+import { Link, Outlet } from 'react-router-dom';
+import style from '../pages/Auth.module.css';
+import { useEffect, useState } from 'react';
+import logo from '../assets/img/logo.png';
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { BsTwitter } from "react-icons/bs";
+
+export function AuthLayout() {
+    const [click, setClick] = useState(() => JSON.parse(localStorage.getItem('logo')) || 0);
+
+    useEffect(() => {
+        localStorage.setItem('logo', JSON.stringify(click));
+    }, [click]);
+
+    function updateCount() {
+        setClick(click + 1);
+    }
+
+    return (
+        <div className={style.start}>
+            <div className={style.logo}>
+                <img onClick={updateCount} src={logo} alt="KFC" />
+                <p>{click}</p>
+            </div>
+            <Outlet />
+            <div className={style.links}>
+                <button><BsFacebook size='50px'/></button>
+                <button><SiGmail size='50px'/></button>
+                <button><BsInstagram size='50px'/></button>
+                <button><BsTwitter size='50px'/></button>
+            </div>
+        </div>
+    );
+}
