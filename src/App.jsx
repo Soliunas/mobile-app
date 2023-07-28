f42f5fde383e181e69c9c32288cbc7e086d420b6
Soliunas <simas.soliunas@gmail.com>
2023-07-28 13:32:51 +0300

    Registration css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { NotFound } from './pages/NotFound';
 import { Start } from './pages/Start';
+import { Registration } from './pages/Registration';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' index element={<Start />} />
+        <Route path='/Registration' element={<Registration />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
-import style from './Start.module.css';
+import style from './Registration.module.css';
 import logo from '../assets/img/logo.png';
-
 
 export function Registration() {
     return (
@@ -13,10 +12,12 @@ export function Registration() {
             </form>
             <p>Or sign in with...</p>
             <div>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button>Facebook</button>
+                <button>Google</button>
+                <button>Instagram</button>
+                <button>Twitter</button>
             </div>
+            <p>Already have an account? <Link to='/login'>Log in</Link></p>
             <Link to='/' className={style.button}>Go home</Link>
         </div>
     )
