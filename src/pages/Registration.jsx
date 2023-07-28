7f5fdbc743caff93f67fb6c07548aa71d0e4a22f
Soliunas <simas.soliunas@gmail.com>
2023-07-28 12:08:23 +0300

    Registration page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -3,12 +3,20 @@ import style from './Start.module.css';
 import logo from '../assets/img/logo.png';
 
 
-export function NotFound() {
+export function Registration() {
     return (
         <div className={style.start}>
             <img className={style.logo} src={logo} alt='KFC' />
-            <h1 className={style.h1}>404</h1>
-            <p>Chicken was not found</p>
+            <h1 className={style.h1}>Sign up</h1>
+            <form>
+
+            </form>
+            <p>Or sign in with...</p>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
             <Link to='/' className={style.button}>Go home</Link>
         </div>
     )
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import style from './Start.module.css';
 import logo from '../assets/img/logo.png';
 
@@ -7,8 +8,9 @@ export function Start() {
         <div className={style.start}>
             <img class Name={style.logo} src={logo} alt='KFC' />
             <h1 className={style.h1}>Welcome</h1>
-            <p>Click below to order some chicken</p>
-            <a className={style.button} href='/'>Take my money</a>
+            <p>Click below to register and order some chicken</p>
+            <Link to='/registration' className={style.button}>Take my money</Link>
+
         </div>
     )
 }
