0b4fd5986be40fdba8e4025e5e2b886dbd847ea6
Soliunas <simas.soliunas@gmail.com>
2023-07-28 14:04:11 +0300

    React icons

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
 import style from './Registration.module.css';
 import logo from '../assets/img/logo.png';
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { BsTwitter } from "react-icons/bs";
 
 export function Registration() {
     return (
         <div className={style.start}>
             <img className={style.logo} src={logo} alt='KFC' />
             <h1 className={style.h1}>Sign up</h1>
-            <form>
+            <form className={style.form}>
 
             </form>
             <p>Or sign in with...</p>
-            <div>
-                <button>Facebook</button>
-                <button>Google</button>
-                <button>Instagram</button>
-                <button>Twitter</button>
+            <div className={style.links}>
+                <button><BsFacebook size='50px' color='fff'/></button>
+                <button><SiGmail size='50px' color='fff'/></button>
+                <button><BsInstagram size='50px' color='fff'/></button>
+                <button><BsTwitter size='50px' color='fff'/></button>
             </div>
             <p>Already have an account? <Link to='/login'>Log in</Link></p>
             <Link to='/' className={style.button}>Go home</Link>
